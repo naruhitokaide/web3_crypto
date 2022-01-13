cf3fb6233f5d15c8a525f866d0e1b491f7718135
KodaKDominus <kodakatsuro@gmail.com>
2022-01-13 11:02:54 +0330

    Remove state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import { Navbar, Footer, Loader, Transactions, Services, Welcome } from './components';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div className='min-h-screen'>
       <div className='gradient-bg-welcome'>
