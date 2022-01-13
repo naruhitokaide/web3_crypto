129fdcedd9e5c42fec86800404ab6df1c14cbdea
KodaKDominus <kodakatsuro@gmail.com>
2022-01-13 12:15:20 +0330

    Import provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { TransactionsProvider } from './context/TransactionContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TransactionsProvider>
+      <App />
+    </TransactionsProvider>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
