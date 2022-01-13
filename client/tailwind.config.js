9a9556bfc75373f8e095d7657afd6cbd01773854
KodaKDominus <kodakatsuro@gmail.com>
2022-01-13 11:03:14 +0330

    Change tailwind configs

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,35 @@
 module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   mode: 'jit',
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ['Open Sans', 'sans-serif'],
+      body: ['Open Sans', 'sans-serif'],
+    },
+    extend: {
+      screens: {
+        mf: '990px',
+      },
+      keyframes: {
+        'slide-in': {
+          '0%': {
+            '-webkit-transform': 'translateX(120%)',
+            transform: 'translateX(120%)',
+          },
+          '100%': {
+            '-webkit-transform': 'translateX(0%)',
+            transform: 'translateX(0%)',
+          },
+        },
+      },
+      animation: {
+        'slide-in': 'slide-in 0.5s ease-out',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 };
