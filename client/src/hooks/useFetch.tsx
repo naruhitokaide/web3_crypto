36a76c61829e013add0c4518efd58f73ce14d7e3
KodaKDominus <kodakatsuro@gmail.com>
2022-01-13 17:35:09 +0330

    Fix save image url

--- client/src/hooks/useFetch.tsx
+++ client/src/hooks/useFetch.tsx
@@ -7,34 +7,35 @@ interface useFetchProps {
 }
 
 const useFetch = ({ keyword }: useFetchProps) => {
-  const [gitUrl, setGitUrl] = useState('');
+  const [gifUrl, setGifUrl] = useState('');
 
   const fetchGifs = async () => {
-    try {
-      const response = await fetch(
-        `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}?q=${keyword
-          .split(' ')
-          .join('')}&limit=1`
-      );
-
-      const { data } = await response.json();
-
-      setGitUrl(data[0].images.downsized_medium?.url);
-    } catch (error) {
-      console.log(error);
-      setGitUrl(
-        'https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284'
-      );
-    }
+    await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword
+        .split(' ')
+        .join('')}&limit=1`
+    )
+      .then((res) => res.json())
+      .then(({ data }) => {
+        setGifUrl(data?.[0]?.images?.downsized_medium?.url);
+        if (!data.length) {
+          setGifUrl('https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setGifUrl('https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif');
+      });
   };
 
   useEffect(() => {
     if (keyword) {
+      console.log('keyword: ', keyword.split(' ').join(''));
       fetchGifs();
     }
   }, [keyword]);
 
-  return gitUrl;
+  return gifUrl;
 };
 
 export default useFetch;
