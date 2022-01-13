befa69f741c5ca35dbb5b3a33e10d84999f2bdae
KodaKDominus <kodakatsuro@gmail.com>
2022-01-13 11:02:05 +0330

    Create loader

--- client/src/components/Loader/Loader.tsx
+++ client/src/components/Loader/Loader.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div>
-      <div>Loader</div>
+    <div className='flex items-center justify-center py-3'>
+      <div className='w-32 h-32 border-b-2 border-red-700 rounded-full animate-spin'></div>
     </div>
   );
 };
